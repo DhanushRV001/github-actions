c4828f237f3ff890b3dd33e7bd96882b2a6bad9b
DhanushRV001 <dhanush.2205021@srec.ac.in>
2025-11-07 16:22:27 +0530

    Composite and input changes

--- .github/actions/greetuser/greet.sh
+++ .github/actions/greetuser/greet.sh
@@ -1,2 +1,10 @@
-echo "$INPUT_NAME who works in $INPUT_ORGANISATION "
+NAME="$1"
+ORG="$2"
+
+if [ -z "$NAME" ] || [ -z "$ORG" ]; then
+  echo "❌ Missing inputs. Usage: greet.sh <name> <organisation>"
+  exit 1
+fi
+
+echo "$NAME who works in $ORG"
 echo "My current branch is: $GITHUB_REF_NAME"
